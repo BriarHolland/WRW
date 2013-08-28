804a0eebc62aab2d490a450a9b74e28084e1ba1d
BriarHolland <briar.holland@gmail.com>
2013-08-28 19:15:46 +1200

    first upload

---
+++ about.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>WRW &#38; CO.</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="geo.region" content="NZ-WKO" />
+        <meta name="geo.placename" content="Raglan" />
+        <meta name="geo.position" content="-37.801406;174.871794" />
+        <meta name="ICBM" content="-37.801406, 174.871794" />
+        <meta name="viewport" content="width=device-width">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <meta content='user-scalable=no, width=device-width' name='viewport'>
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="style.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <!--[if lt IE 7]><link rel="stylesheet" href="css/ie.css"><![endif]-->
+    </head>
+    <body>
+    <div class="wrapper">
+    <header>
+    <nav class="mainNav">
+        <ul> 
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">COLLECTION</a></li>
+        <li><a href="#"><img src="logo.jpg" alt="logo" /></a></li>
+        <li><a href="#">PRESS</a></li>
+        <li><a href="#">GALLERY</a></li>
+        <li><a href="#">CONTACT</a></li>
+        </ul>
+    </nav>
+    <header>
+
+    <div class="about">
+    <h2>ABOUT</h2>
+        <div class="leftCol">
+            <h3>An Interview with our Table Maker - Will from WRW and Co.</h3>
+            <h5>The tables you are making are stunning. Do you mainly use recycled materials?</h5>
+            <p>Yes i use a lot of reclaimed timber - for one it’s a fantastic sustainable option and also
+            with reclaimed timber they carry a history of a former life, I love timber that is full of
+            character and 'historic scars'. These are all exposed carefully in the process of repurposing
+            them into a new piece - dining tables, hallway tables, coffee tables, cafe tables.</p>
+            <h5>When did you start making your own furniture? And what inspires you?</h5>
+            <p>My inspiration is fueled by a deep appreciation of craftsmen before me whose skilled
+            hands and sometimes simple tools created buildings and furniture which have survived
+            many decades not only environmentally but also aesthetically where pragmatism and
+            style come together in a perfect balance. It's the enjoyment in creating something unique
+            with its own story and character, being able to see the historical scars in the timber, and
+            the excitement on the clients face when I've delivered a table.</p>
+            <p>I have always had a fascination with furniture and built my first table from reclaimed kauri
+            4 years ago for the Bach in Raglan. My first effort resulted in a flurry of orders. With the
+            will and the skill but lack of a formal workshop I managed to complete the orders in the
+            wool shed with equipment rustled up from toolboxes around the sheds. A few sold quickly
+            in one of Ponsonbys top design stores. I then returned to my job in London as a project
+            manager for interior fit outs of restaurants, shops and offices.</p>
+            <h5>Raglan for Will Worsp is...</h5>
+            <p>Raglan for me feels like home.. From a farm in Te Akau Raglan has always been a place of
+            interest and while i've been away for a while it's nice to have seen it grow in its popularity
+            over the years but still keep its strong nit community and surf town values, it's NZs
+            coolest little town!! Best coffee, best waves, best town! Raglan has got a bit of everything!
+            It’s obvious to see by the hordes of cars heading up or down for the weekends.</p>
+        </div><!--/leftCol-->
+        
+        <div class="rightCol">
+        <h3>About Me.</h3>
+            <p>Akau Raglan has always been a place of interest
+            and while i've been away for a while it's nice to
+            have seen it grow in its popularity over the years
+            but still keep its strong nit community and surf
+            town values, it's NZs
+            coolest little town!! Best coffee, best waves, best
+            town! Raglan has got a bit of everything! It’s obvious
+            to see by the hordes of cars heading up or
+            down for the weekends.</p>
+        </div><!--/rightCol-->
+    </div><!--/about-->
+
+    <div class="contact">
+        <div class="leftCol">
+            <h3>CONTACT</h3>
+            <h5>Contact</h5>
+            <p>Please contact me for any specific requests or to discuss delivery</p>
+            <h5>William Worsp</h5>
+            <p>021 000 000<p>
+            <p>wrworsp&#64;gmail.com</p>
+            <div class="contact_form">
+            </div><!--/contact_form-->
+        </div><!--/leftCol-->
+
+        <div class="socialMedia">
+            <h5>Social Media</h5>
+            <a href="http://facebook.com" target="_blank">FACEBOOK</a>
+            <a href="http://pinterest.com" target="_blank">PINTEREST</a>
+        </div><!--/socialMedia-->
+
+        <div class="rightCol">
+        <h5>Order</h5>
+        <p>Please use this order form to place an order.</p>
+        <div class="order_form">
+        </div><!--/order_form-->
+        </div><!--/rightCol-->
+
+    </div><!--/contact-->
+
+        <footer>
+        <p>&#169;2013 WRW &#38; CO.</p>
+        <img src="" alt="" />
+        <a href="http://pollencollective.co.nz" target="_blank"><img src="" alt="" />Pollen</a>
+        </footer>
+    </div><!--/wrapper-->
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+        <script src="js/vendor/jquery.easing-1.3.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
---
+++ contact.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <title>WRW &#38; CO.</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="geo.region" content="NZ-WKO" />
+        <meta name="geo.placename" content="Raglan" />
+        <meta name="geo.position" content="-37.801406;174.871794" />
+        <meta name="ICBM" content="-37.801406, 174.871794" />
+        <meta name="viewport" content="width=device-width">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <meta content='user-scalable=no, width=device-width' name='viewport'>
+        <link rel="stylesheet" href="css/normalize.min.css">
+        <link rel="stylesheet" href="style.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+        <!--[if lt IE 7]><link rel="stylesheet" href="css/ie.css"><![endif]-->
+    </head>
+    <body>
+    <div class="wrapper">
+    <header>
+    <nav class="mainNav">
+        <ul> 
+        <li><a href="#">HOME</a></li>
+        <li><a href="#">ABOUT</a></li>
+        <li><a href="#">COLLECTION</a></li>
+        <li><a href="#"><img src="logo.jpg" alt="logo" /></a></li>
+        <li><a href="#">PRESS</a></li>
+        <li><a href="#">GALLERY</a></li>
+        <li><a href="#">CONTACT</a></li>
+        </ul>
+    </nav>
+    <header>
+
+
+        <footer>
+        <p>&#169;2013 WRW &#38; CO.</p>
+        <img src="" alt="" />
+        <a href="http://pollencollective.co.nz" target="_blank"><img src="" alt="" /></a>
+        </footer>
+    </div><!--/wrapper-->
+
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+		<script src="js/vendor/jquery.easing-1.3.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
