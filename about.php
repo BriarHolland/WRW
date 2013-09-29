3248ebc2a663d584cb607db00859d670bd8c58a6
Jansbury <Jansbury@jansburyPollen.local>
2013-09-30 12:27:04 +1300

    Tidy Up

--- about.php
+++ about.php
@@ -9,47 +9,7 @@
    <div class="about clearfix">
 
            <div class="leftCol">
-               <div class="interTitle">An Interview with our Table Maker - Will from WRW and Co.</div>
-               
-               <div class="interQ">The tables you are making are stunning. Do you mainly use recycled materials?</div>
-               
-               <p>Yes i use a lot of reclaimed timber - for one it’s a fantastic sustainable option and also
-               with reclaimed timber they carry a history of a former life, I love timber that is full of
-               character and 'historic scars'. These are all exposed carefully in the process of repurposing
-               them into a new piece - dining tables, hallway tables, coffee tables, cafe tables.</p>
-               
-               <div class="interQ">When did you start making your own furniture? And what inspires you?</div>
-               
-               <p>My inspiration is fuelled by a deep appreciation of craftsmen before me whose skilled
-               hands and sometimes simple tools created buildings and furniture which have survived
-               many decades not only environmentally but also aesthetically where pragmatism and
-               style come together in a perfect balance. It's the enjoyment in creating something unique
-               with its own story and character, being able to see the historical scars in the timber, and
-               the excitement on the clients face when I've delivered a table.</p>
-
-               <p>I have always had a fascination with furniture and built my first table from reclaimed kauri
-               4 years ago for the Bach in Raglan. My first effort resulted in a flurry of orders. With the
-               will and the skill but lack of a formal workshop I managed to complete the orders in the
-               wool shed with equipment rustled up from toolboxes around the sheds. A few sold quickly
-               in one of Ponsonby's top design stores. I then returned to my job in London as a project
-               manager for interior fit outs of restaurants, shops and offices.</p>
-
-               <div class="interQ">Raglan for Will Worsp is...</div>
-               
-               <p>Raglan for me feels like home.. From a farm in Te Akau Raglan has always been a place of
-               interest and while i've been away for a while it's nice to have seen it grow in its popularity
-               over the years but still keep its strong nit community and surf town values, it's NZ's
-               coolest little town!! Best coffee, best waves, best town! Raglan has got a bit of everything!
-               It’s obvious to see by the hordes of cars heading up or down for the weekends.</p>
-           </div><!--/leftCol-->
-
-           <div class="rightCol">
-               
-                   
-                   <div class="aboutMeImg">
-	                   <img src="i/aboutMe.jpg" alt="aboutme"/>
-                   </div>
-                  <h3>About Me.</h3> 
+                <h3>About William Worsp</h3> 
                    <p>Akau Raglan has always been a place of interest
                    and while i've been away for a while it's nice to
                    have seen it grow in its popularity over the years
@@ -61,6 +21,15 @@
                    to see by the hordes of cars heading up or
                    down for the weekends.</p>
 
+           </div><!--/leftCol-->
+
+           <div class="rightCol">
+               
+                   
+                   <div class="aboutMeImg">
+	                   <img src="i/aboutMe.jpg" alt="aboutme"/>
+                   </div>
+                 
            </div><!--/rightCol-->
    </div><!--/about-->
 
--- press.php
+++ press.php
@@ -10,13 +10,25 @@
    
    		<div class="pressCon">
 	   		<h3>Press Title</h3>
+		 		<h5 class="source">
+	   			
+	   		</h5>
 	   		<div class="pressImg clearfix">
-		   		<img src="" alt=""/>
+		   		<img src="i/press/press_01.jpg" alt="tableTop"/>
 	   		</div>
 	   		<div class="pressText clearfix">
-		   		Akau Raglan has always been a place of interest and while i've been away for a while it's nice to have seen it grow in its popularity over the years but still keep its strong nit community and surf town values, it's NZs coolest little town!! Best coffee, best waves, best town! Raglan has got a bit of everything! It’s obvious to see by the hordes of cars heading up or down for the weekends.
+		   		I design and build furniture from formed and repurposed materials, I also work in interior design. I have always had a fascination with furniture and built my first table from reclaimed kauri 4 years ago for the bach in Raglan. My first effort resulted in a flurry of orders. With the will and the skill but lack of a formal workshop I managed to complete the orders in the woolshed with equipment russelled up from toolboxes around the sheds. A few sold quickly in one of Ponsonby’s top design stores. I then returned to my job in London as a project manager for interior fitouts of restaurants, shops and offices.
+		   		
+		   		<div class="origCon">
+		   			<a href class="origLink">View the original post</a>
+		   		</div>
+	   		
 	   		</div>
    		</div>
+   		
+   		
+   		
+   		
    		
    		<div class="pressCon">
 	   		<h3>Press Title</h3>
@@ -27,6 +39,12 @@
 		   		Akau Raglan has always been a place of interest and while i've been away for a while it's nice to have seen it grow in its popularity over the years but still keep its strong nit community and surf town values, it's NZs coolest little town!! Best coffee, best waves, best town! Raglan has got a bit of everything! It’s obvious to see by the hordes of cars heading up or down for the weekends.
 	   		</div>
    		</div>
+   		
+   		
+   		
+   		
+   		
+   		
    		
    		<div class="pressCon">
 	   		<h3>Press Title</h3>
